b7d9dc0a8e92c78a3116ee2fb1945ac74be4e81d
season <3090331318@qq.com>
2025-03-01 21:21:24 +0800

    feat: 帖子编辑优化

--- app/edit/index.tsx
+++ app/edit/index.tsx
@@ -1,17 +1,37 @@
 import { ThemedSafeAreaView } from "@/components/ui/ThemedSafeAreaView";
 import * as ImagePicker from "expo-image-picker";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TextInput,
+  StyleSheet,
+  TouchableNativeFeedback,
+  ToastAndroid,
+} from "react-native";
 import * as FileSystem from "expo-file-system";
 import { requestPermission } from "@/utils/permission";
+import { AntDesign } from "@expo/vector-icons";
+import { vw } from "@/utils";
+import { Show } from "@/components/ui/Show";
 
 type EditPageParams = {
   uri: string;
 };
 export default function EditPage() {
-  const [selectedImage, setSelectedImage] = useState<string[]>([]);
   const params = useLocalSearchParams<EditPageParams>();
+  const [selectedImage, setSelectedImage] = useState<string[]>([]);
+  const [content, setContent] = useState<string>("");
+
+  const publish = async () => {
+    router.dismissAll();
+    router.push({
+      pathname: "/",
+    });
+  };
 
   const pickImage = async () => {
     const hasPermission = await requestPermission();
@@ -31,12 +51,12 @@ export default function EditPage() {
   };
 
   useEffect(() => {
+    if (!params.uri || selectedImage.length) return;
     FileSystem.getInfoAsync(params.uri).then((info) => {
       if (info.exists) {
-        console.log("文件存在");
         setSelectedImage([...selectedImage, params.uri]);
       } else {
-        console.log("文件不存在");
+        ToastAndroid.show("图片不存在", ToastAndroid.SHORT);
       }
     });
   }, [params.uri]);
@@ -46,11 +66,32 @@ export default function EditPage() {
   }, []);
   return (
     <ThemedSafeAreaView style={styles.container}>
-      <Button title="从相册选择图片" onPress={pickImage} />
-      {selectedImage &&
-        selectedImage.map((uri, index) => (
-          <Image key={index} source={{ uri }} style={styles.image} />
-        ))}
+      {/* 图片区域 */}
+      <View style={styles.gallery}>
+        <Show when={!!selectedImage}>
+          {selectedImage.map((uri, index) => (
+            <Image key={index} source={{ uri }} style={styles.image} />
+          ))}
+        </Show>
+        <Show when={selectedImage.length < 9}>
+          <TouchableNativeFeedback onPress={pickImage}>
+            <View style={styles.addImage}>
+              <AntDesign name="plus" size={24} color="#ccc" />
+            </View>
+          </TouchableNativeFeedback>
+        </Show>
+      </View>
+      <View>
+        <TextInput
+          placeholder="添加正文"
+          cursorColor={"red"}
+          onChangeText={setContent}
+        />
+      </View>
+      {/* 底部栏 */}
+      <View style={styles.bottomBar}>
+        <Button title="发布" onPress={publish} />
+      </View>
     </ThemedSafeAreaView>
   );
 }
@@ -59,9 +100,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  gallery: {
+    width: vw(396),
+    marginHorizontal: "auto",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: vw(10),
+  },
   image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
+    width: vw(120),
+    height: vw(120),
+    borderRadius: 8,
+  },
+  addImage: {
+    width: vw(120),
+    height: vw(120),
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  bottomBar: {
+    marginTop: "auto",
+    padding: vw(16),
   },
 });
